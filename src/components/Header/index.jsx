16d5a30ef71081ba24979a26fe52391f9fe48af0
a-mariam <mariiam22222@gmail.com>
2023-10-10 17:26:17 +0100

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Remotes,SmallRoundButton } from '../components/Header/styles';
+function Remote(){
+    return(
+        <Remotes>
+          <SmallRoundButton></SmallRoundButton>
+        </Remotes>
+    )
+}
+export default Remote;
